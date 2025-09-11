810270db353b38a957de447d1c3a40c3721a60b5
Zecky-dev <l2021012041@ogr.sdu.edu.tr>
2025-09-11 21:58:25 +0300

    Refactor Firebase initialization to use environment variables for credentials

--- config/firebase.js
+++ config/firebase.js
@@ -3,9 +3,12 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   try {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        projectId: process.env.FIREBASE_PROJECT_ID,
+      }),
     });
   } catch (error) {
     console.error("Firebase Admin SDK başlatılırken hata oluştu:", error);
